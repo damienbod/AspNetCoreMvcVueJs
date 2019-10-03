034e41737b5e034b414a00a3c649d5ae26137916
damienbod <damien_bod@hotmail.com>
2019-10-03 15:50:54 +0200

    adding a wwwroot build

--- AspNetCoreMvcVueJs/vuejs/vue.config.js
+++ AspNetCoreMvcVueJs/vuejs/vue.config.js
@@ -1,6 +1,7 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    outputDir: '../wwwroot',
     configureWebpack: {
       plugins: [
         new CopyWebpackPlugin([
