ac4e82b9c8f91be96368a78b03cb97b494584ab2
damienbod <damien_bod@hotmail.com>
2020-09-12 19:02:46 +0200

    updating startup

--- StsServerIdentity/Startup.cs
+++ StsServerIdentity/Startup.cs
@@ -4,144 +4,163 @@ using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
-using IdentityServer4.Services;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
-using Serilog;
-using StsServerIdentity.Data;
-using StsServerIdentity.Filters;
+using IdentityServer4.Services;
 using StsServerIdentity.Models;
+using StsServerIdentity.Data;
 using StsServerIdentity.Resources;
 using StsServerIdentity.Services;
+using StsServerIdentity.Filters;
 using StsServerIdentity.Services.Certificate;
+using Serilog;
+using Microsoft.AspNetCore.Http;
+using Fido2NetLib;
 
 namespace StsServerIdentity
 {
     public class Startup
     {
+        private IConfiguration _configuration { get; }
+        private IWebHostEnvironment _environment { get; }
+
         public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             _configuration = configuration;
             _environment = env;
         }
 
-        public IConfiguration _configuration { get; }
-        public IWebHostEnvironment _environment { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<AuthConfiguration>(_configuration.GetSection("AuthConfiguration"));
-            services.Configure<AuthSecretsConfiguration>(_configuration.GetSection("AuthSecretsConfiguration"));
+            services.Configure<StsConfig>(_configuration.GetSection("StsConfig"));
             services.Configure<EmailSettings>(_configuration.GetSection("EmailSettings"));
             services.AddTransient<IProfileService, IdentityWithAdditionalClaimsProfileService>();
             services.AddTransient<IEmailSender, EmailSender>();
 
-            var authConfiguration = _configuration.GetSection("AuthConfiguration");
-            var authSecretsConfiguration = _configuration.GetSection("AuthSecretsConfiguration");
-
-            var clientId = _configuration["MicrosoftClientId"];
-            var clientSecret = _configuration["MircosoftClientSecret"];
-
-            var sharedResourceAssemblyName = SharedResourceAssemblyName;
-
-            var x509Certificate2 = GetCertificate(_environment, _configuration);
-
-            var vueJsApiUrl = authConfiguration["VueJsApiUrl"];
-
-            services.AddCors(options =>
+            services.Configure<CookiePolicyOptions>(options =>
             {
-                options.AddPolicy("AllowAllOrigins",
-                    builder =>
-                    {
-                        builder
-                            .AllowCredentials()
-                            .WithOrigins(vueJsApiUrl)
-                            .SetIsOriginAllowedToAllowWildcardSubdomains()
-                            .AllowAnyHeader()
-                            .AllowAnyMethod();
-                    });
+                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+                options.OnAppendCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                options.OnDeleteCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
             });
+
+            var x509Certificate2Certs = GetCertificates(_environment, _configuration)
+                .GetAwaiter().GetResult();
+            AddLocalizationConfigurations(services);
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddSingleton<LocService>();
-            AddLocalizationConfigurations(services, sharedResourceAssemblyName);
-
             services.AddIdentity<ApplicationUser, IdentityRole>()
-               .AddEntityFrameworkStores<ApplicationDbContext>()
-               .AddErrorDescriber<StsIdentityErrorDescriber>()
-               .AddDefaultTokenProviders();
-
-            // services.AddAuthorization();     => Don't need this as it is include with .AddControllersWithViews
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddErrorDescriber<StsIdentityErrorDescriber>()
+                .AddDefaultTokenProviders()
+                .AddTokenProvider<Fifo2UserTwoFactorTokenProvider>("FIDO2");
 
             services.AddAuthentication()
-                 .AddOpenIdConnect("aad", "Login with Azure AD", options =>
+                 .AddOpenIdConnect("aad", "Login with Azure AD", options => // Microsoft common
                  {
-                     options.Authority = $"https://login.microsoftonline.com/common";
-                     options.TokenValidationParameters = new TokenValidationParameters { ValidateIssuer = false };
-                     options.ClientId = "99eb0b9d-ca40-476e-b5ac-6f4c32bfb530";
-                     options.CallbackPath = "/signin-oidc";
+                     //  https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration
+                     options.ClientId = "your_client_id"; // ADD APP Registration ID
+                     options.ClientSecret = "your_secret"; // ADD APP Registration secret
+                     options.SignInScheme = "Identity.External";
+                     options.RemoteAuthenticationTimeout = TimeSpan.FromSeconds(30);
+                     options.Authority = "https://login.microsoftonline.com/common/v2.0/";
+                     options.ResponseType = "code";
+                     options.UsePkce = false; // live does not support this yet
+                     options.Scope.Add("profile");
+                     options.Scope.Add("email");
+                     options.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         // ALWAYS VALIDATE THE ISSUER IF POSSIBLE !!!!
+                         ValidateIssuer = false,
+                         // ValidIssuers = new List<string> { "tenant..." },
+                         NameClaimType = "email",
+                     };
+                     options.CallbackPath = "/signin-microsoft";
+                     options.Prompt = "login"; // login, consent
                  });
+
+            services.AddAntiforgery(options =>
+            {
+                options.SuppressXFrameOptionsHeader = true;
+                options.Cookie.SameSite = SameSiteMode.Strict;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            });
 
             services.AddControllersWithViews(options =>
                 {
                     options.Filters.Add(new SecurityHeadersAttribute());
                 })
-                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
                 .AddViewLocalization()
                 .AddDataAnnotationsLocalization(options =>
                 {
                     options.DataAnnotationLocalizerProvider = (type, factory) =>
                     {
-                        return factory.Create("SharedResource", sharedResourceAssemblyName);
+                        var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
+                        return factory.Create("SharedResource", assemblyName.Name);
                     };
-                });
+                })
+                .AddNewtonsoftJson();
 
-            services.AddRazorPages();
+            var stsConfig = _configuration.GetSection("StsConfig");
 
-            services.AddIdentityServer()
-                .AddSigningCredential(x509Certificate2)
+            var identityServer = services.AddIdentityServer()
+                .AddSigningCredential(x509Certificate2Certs.ActiveCertificate)
                 .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryApiResources(Config.GetApiResources(authSecretsConfiguration))
-                .AddInMemoryClients(Config.GetClients(authConfiguration))
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryApiScopes(Config.GetApiScopes())
+                .AddInMemoryClients(Config.GetClients(stsConfig))
                 .AddAspNetIdentity<ApplicationUser>()
                 .AddProfileService<IdentityWithAdditionalClaimsProfileService>();
+
+            if (x509Certificate2Certs.SecondaryCertificate != null)
+            {
+                identityServer.AddValidationKey(x509Certificate2Certs.SecondaryCertificate);
+            }
+
+            services.Configure<Fido2Configuration>(_configuration.GetSection("fido2"));
+            services.AddScoped<Fido2Storage>();
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(2);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            });
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseCookiePolicy();
+
             if (_environment.IsDevelopment())
             {
-                IdentityModelEventSource.ShowPII = true;
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());
+                app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseCors("AllowAllOrigins");
-
+            app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains());
             app.UseXContentTypeOptions();
             app.UseReferrerPolicy(opts => opts.NoReferrer());
             app.UseXXssProtection(options => options.EnabledWithBlockMode());
-
-            var authConfiguration = _configuration.GetSection("AuthConfiguration");
-            var vueJsApiUrl = authConfiguration["VueJsApiUrl"];
 
             app.UseCsp(opts => opts
                 .BlockAllMixedContent()
@@ -149,9 +168,6 @@ namespace StsServerIdentity
                 .StyleSources(s => s.UnsafeInline())
                 .FontSources(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .FrameAncestors(s => s.CustomSources(
-                   vueJsApiUrl)
-                 )
                 .ImageSources(imageSrc => imageSrc.Self())
                 .ImageSources(imageSrc => imageSrc.CustomSources("data:"))
                 .ScriptSources(s => s.Self())
@@ -160,6 +176,10 @@ namespace StsServerIdentity
 
             var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
             app.UseRequestLocalization(locOptions.Value);
+
+            // https://nblumhardt.com/2019/10/serilog-in-aspnetcore-3/
+            // https://nblumhardt.com/2019/10/serilog-mvc-logging/
+            app.UseSerilogRequestLogging();
 
             app.UseStaticFiles(new StaticFileOptions()
             {
@@ -181,68 +201,48 @@ namespace StsServerIdentity
                 }
             });
 
-            app.UseIdentityServer();
-
-            // https://nblumhardt.com/2019/10/serilog-in-aspnetcore-3/
-            // https://nblumhardt.com/2019/10/serilog-mvc-logging/
-            app.UseSerilogRequestLogging();
-
             app.UseRouting();
 
-            app.UseAuthentication();
+            app.UseIdentityServer();
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
             });
         }
 
-        private static X509Certificate2 GetCertificate(IWebHostEnvironment environment, IConfiguration configuration)
+        private static async Task<(X509Certificate2 ActiveCertificate, X509Certificate2 SecondaryCertificate)> GetCertificates(IWebHostEnvironment environment, IConfiguration configuration)
         {
-            X509Certificate2 cert;
-            var useLocalCertStore = Convert.ToBoolean(configuration["UseLocalCertStore"]);
-            var certificateThumbprint = configuration["CertificateThumbprint"];
-
-            if (environment.IsProduction())
+            var certificateConfiguration = new CertificateConfiguration
             {
-                if (useLocalCertStore)
-                {
-                    using (X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
-                    {
-                        store.Open(OpenFlags.ReadOnly);
-                        var certs = store.Certificates.Find(X509FindType.FindByThumbprint, certificateThumbprint, false);
-                        cert = certs[0];
-                        store.Close();
-                    }
-                }
-                else
-                {
-                    // Azure deployment, will be used if deployed to Azure
-                    var vaultConfigSection = configuration.GetSection("Vault");
-                    var keyVaultService = new KeyVaultCertificateService(vaultConfigSection["Url"], vaultConfigSection["ClientId"], vaultConfigSection["ClientSecret"]);
-                    cert = keyVaultService.GetCertificateFromKeyVault(vaultConfigSection["CertificateName"]);
-                }
-            }
-            else
-            {
-                cert = new X509Certificate2(Path.Combine(environment.ContentRootPath, "sts_dev_cert.pfx"), "1234");
-            }
+                // Use an Azure key vault
+                CertificateNameKeyVault = configuration["CertificateNameKeyVault"], //"StsCert",
+                KeyVaultEndpoint = configuration["AzureKeyVaultEndpoint"], // "https://damienbod.vault.azure.net"
 
-            return cert;
+                // Use a local store with thumbprint
+                //UseLocalCertStore = Convert.ToBoolean(configuration["UseLocalCertStore"]),
+                //CertificateThumbprint = configuration["CertificateThumbprint"],
+
+                // development certificate
+                DevelopmentCertificatePfx = Path.Combine(environment.ContentRootPath, "sts_dev_cert.pfx"),
+                DevelopmentCertificatePassword = "1234" //configuration["DevelopmentCertificatePassword"] //"1234",
+            };
+
+            (X509Certificate2 ActiveCertificate, X509Certificate2 SecondaryCertificate) certs = await CertificateService.GetCertificates(
+                certificateConfiguration).ConfigureAwait(false);
+
+            return certs;
         }
 
-        private static void AddLocalizationConfigurations(IServiceCollection services, string sharedResourceAssemblyName)
+        private static void AddLocalizationConfigurations(IServiceCollection services)
         {
+            services.AddSingleton<LocService>();
             services.AddLocalization(options => options.ResourcesPath = "Resources");
-
-            services.AddSingleton(sp => {
-                var factory = sp.GetRequiredService<IStringLocalizerFactory>();
-                return factory.Create("SharedResource", sharedResourceAssemblyName);
-            });
 
             services.Configure<RequestLocalizationOptions>(
                 options =>
@@ -255,7 +255,9 @@ namespace StsServerIdentity
                             new CultureInfo("it-IT"),
                             new CultureInfo("gsw-CH"),
                             new CultureInfo("fr-FR"),
-                            new CultureInfo("zh-Hans")
+                            new CultureInfo("zh-Hans"),
+                            new CultureInfo("ga-IE"),
+                            new CultureInfo("es-MX")
                         };
 
                     options.DefaultRequestCulture = new RequestCulture(culture: "de-DE", uiCulture: "de-DE");
@@ -271,6 +273,52 @@ namespace StsServerIdentity
                 });
         }
 
-        private static string SharedResourceAssemblyName => typeof(SharedResource).Assembly.FullName;
+        private static void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+            if (options.SameSite == SameSiteMode.None)
+            {
+                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+                if (DisallowsSameSiteNone(userAgent))
+                {
+                    // For .NET Core < 3.1 set SameSite = (SameSiteMode)(-1)
+                    options.SameSite = SameSiteMode.Unspecified;
+                }
+            }
+        }
+
+        private static bool DisallowsSameSiteNone(string userAgent)
+        {
+            // Cover all iOS based browsers here. This includes:
+            // - Safari on iOS 12 for iPhone, iPod Touch, iPad
+            // - WkWebview on iOS 12 for iPhone, iPod Touch, iPad
+            // - Chrome on iOS 12 for iPhone, iPod Touch, iPad
+            // All of which are broken by SameSite=None, because they use the iOS networking stack
+            if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
+            {
+                return true;
+            }
+
+            // Cover Mac OS X based browsers that use the Mac OS networking stack. This includes:
+            // - Safari on Mac OS X.
+            // This does not include:
+            // - Chrome on Mac OS X
+            // Because they do not use the Mac OS networking stack.
+            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
+                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+            {
+                return true;
+            }
+
+            // Cover Chrome 50-69, because some versions are broken by SameSite=None,
+            // and none in this range require it.
+            // Note: this covers some pre-Chromium Edge versions,
+            // but pre-Chromium Edge does not require SameSite=None.
+            if (userAgent.Contains("Chrome/5") || userAgent.Contains("Chrome/6"))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
